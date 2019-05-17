98862667409c7d947f6cf3b74c9eb34779d34538
DarkNeon135 <47756626+DarkNeon135@users.noreply.github.com>
2019-05-17 20:59:22 +0300

    Add files via upload

--- DataFromFile.cs
+++ DataFromFile.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ExtraTask
 {
@@ -7,12 +10,26 @@ namespace ExtraTask
     {
         public static bool ReadFromFile(string timePeriod, string fileNumber, out List<string> readDataList)
         {
-            readDataList = null;
-            return true;
+            try
+            {
+                readDataList = new List<string>();
+                string [] twofolder = timePeriod.Split(' ');
+                twofolder[1] = twofolder[1].Substring(0, twofolder[1].Length - 2);
+                StreamReader analFileStream = new StreamReader($@"\DataToAnalyze\{twofolder[0]}\{twofolder[1]}\{fileNumber}.csv");
+                
+                return true;
+
+            }
+            catch (Exception)
+            {
+                readDataList = null;
+                return false;
+            }
         }
 
-        public static List<double> ParseSelectedColumn(int columnID, List<string> listWithInputData)
+        public static SortedList<double, double> ParseSelectedColumn(int columnID, List<string> listWithInputData)
         {
+            
             return null;
         }
     }
