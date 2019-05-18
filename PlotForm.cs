af40bc28403f63770ea4aa13116abee0220d6a0b
MangoStunt <50739312+MangoStunt@users.noreply.github.com>
2019-05-18 23:48:09 +0300

    Update PlotForm.cs

--- PlotForm.cs
+++ PlotForm.cs
@@ -1,14 +1,7 @@
-﻿using System;
+Volodymyr Burdeinyi, [18.05.19 23:41]
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Accord.Statistics;
-using Accord.Statistics.Models.Regression.Linear;
 using OxyPlot;
 using OxyPlot.Series;
 
@@ -17,52 +10,54 @@ namespace ExtraTask
 {
     public partial class Plot_Form : Form
     {
-        public List<string> InputDataList;
+        private List<string> InputDataList;
+        private DataSelection_Form MainFormObj;
 
         public Plot_Form()
         {
             InitializeComponent();
         }
 
-        public Plot_Form(List<string> list)
+        public Plot_Form(List<string> list, DataSelection_Form obj)
         {
             InitializeComponent();
             InputDataList = list;
+            MainFormObj = obj;
         }
 
+        public PlotModel model = new PlotModel { Title = "Plot" };
+        
         private void PlotBasedOn_comboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            PlotModel model = new PlotModel { Title = "Plot" };
+            LineSeries dots = new LineSeries
+            {
+                LineStyle = LineStyle.None,
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 1.5,
+                //MarkerStroke = OxyColors.Black
+            };
+
+            PoleReg_PlotView.Model = model;
+
             double[] coefForFunc = Algorithm.PolynomialRegresion(PlotBasedOn_comboBox.SelectedItem.ToString(), InputDataList, out SortedList<double, double> variableForPlotsList);
 
             Func<double, double> func = x => coefForFunc[1] * x + coefForFunc[0];
 
             model.Series.Add(new FunctionSeries(func, variableForPlotsList.Keys[0], variableForPlotsList.Keys[variableForPlotsList.Count-1], 0.1));
 
-            LineSeries dots = new LineSeries
-            {
-                LineStyle = LineStyle.None,
-                MarkerType = MarkerType.Circle,
-                MarkerSize = 1.5,
-                MarkerStroke = OxyColors.Black
-            };
             foreach (var pair in variableForPlotsList)
             {
                 dots.Points.Add(new DataPoint(pair.Key, pair.Value));
                 dots.Smooth = true;
-
-                
+ 
             }
             model.Series.Add(dots);
             PoleReg_PlotView.Model = model;
-
         }
-        public PlotModel model = new PlotModel { Title = "Plot" };
 
         private void Plot_Form_Load(object sender, EventArgs e)
         {
-
-   
-
             string[] comboBoxComponent = new string[]
             {
                 "CPU cores",
@@ -79,9 +74,9 @@ namespace ExtraTask
             PlotBasedOn_comboBox.Items.AddRange(comboBoxComponent);
         }
 
-        private void PlotPR_pannel_Paint(object sender, PaintEventArgs e)
+        private void Plot_Form_FormClosed(object sender, FormClosedEventArgs e)
         {
-
+            MainFormObj.Close();
         }
     }
 }
