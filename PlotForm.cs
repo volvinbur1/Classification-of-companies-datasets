99c13e92a054d4646a914caf57dbfcabeff37f75
MangoStunt <50739312+MangoStunt@users.noreply.github.com>
2019-05-18 05:12:41 +0300

    Add files via upload

--- PlotForm.cs
+++ PlotForm.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Accord.Statistics;
+using Accord.Statistics.Models.Regression.Linear;
+using OxyPlot;
+using OxyPlot.Series;
+
 
 namespace ExtraTask
 {
@@ -27,7 +32,50 @@ namespace ExtraTask
 
         private void PlotBasedOn_comboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            Algorithm.PolynomialRegresion(PlotBasedOn_comboBox.SelectedItem.ToString(), InputDataList, out SortedList<double, double> variableForPlotsList);
+            var dots = new OxyPlot.Series.LineSeries
+            {
+                LineStyle = LineStyle.None,
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4,
+                MarkerStroke = OxyColors.Black
+            };
+            foreach (var pair in variableForPlotsList)
+            {
+                dots.Points.Add(new DataPoint(pair.Key, pair.Value));
+                dots.Smooth = true;
 
+                model.Series.Add(dots);
+            }
+
+        }
+        public PlotModel model = new PlotModel { Title = "Plot" };
+
+        private void Plot_Form_Load(object sender, EventArgs e)
+        {
+ 
+            var dots = new OxyPlot.Series.LineSeries
+            {
+                LineStyle = LineStyle.None,
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4.5,
+                MarkerStroke = OxyColors.Black
+            };
+
+            string[] comboBoxComponent = new string[]
+            {
+                "CPU cores",
+                "CPU capacity provisioned [MHZ]",
+                "CPU usage [MHZ]", "CPU usage [%]",
+                "Memory capacity provisioned [KB]",
+                "Memory usage [KB]",
+                "Disk read throughput [KB/s]",
+                "Disk write throughput [KB/s];",
+                "Network received throughput [KB/s]",
+                "Network transmitted throughput [KB/s]"
+            };
+            
+            PlotBasedOn_comboBox.Items.AddRange(comboBoxComponent);
         }
     }
 }
