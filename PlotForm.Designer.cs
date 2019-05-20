8e432b066cb6ea1c327cc7574f3c0cf3670a01ef
MangoStunt <50739312+MangoStunt@users.noreply.github.com>
2019-05-20 21:03:44 +0300

    Add files via upload

--- PlotForm.Designer.cs
+++ PlotForm.Designer.cs
@@ -28,51 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.PlotBasedOn_comboBox = new System.Windows.Forms.ComboBox();
-            this.PoleReg_PlotView = new OxyPlot.WindowsForms.PlotView();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.Size = new System.Drawing.Size(107, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Plot based on...";
             // 
             // PlotBasedOn_comboBox
             // 
             this.PlotBasedOn_comboBox.FormattingEnabled = true;
-            this.PlotBasedOn_comboBox.Location = new System.Drawing.Point(99, 12);
+            this.PlotBasedOn_comboBox.Location = new System.Drawing.Point(132, 15);
+            this.PlotBasedOn_comboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PlotBasedOn_comboBox.Name = "PlotBasedOn_comboBox";
-            this.PlotBasedOn_comboBox.Size = new System.Drawing.Size(121, 21);
+            this.PlotBasedOn_comboBox.Size = new System.Drawing.Size(160, 24);
             this.PlotBasedOn_comboBox.TabIndex = 1;
             this.PlotBasedOn_comboBox.Text = "Select";
             this.PlotBasedOn_comboBox.SelectedIndexChanged += new System.EventHandler(this.PlotBasedOn_comboBox_SelectedIndexChanged);
             // 
-            // PoleReg_PlotView
+            // zedGraphControl1
             // 
-            this.PoleReg_PlotView.Location = new System.Drawing.Point(15, 60);
-            this.PoleReg_PlotView.Margin = new System.Windows.Forms.Padding(2);
-            this.PoleReg_PlotView.Name = "PoleReg_PlotView";
-            this.PoleReg_PlotView.PanCursor = System.Windows.Forms.Cursors.Hand;
-            this.PoleReg_PlotView.Size = new System.Drawing.Size(765, 425);
-            this.PoleReg_PlotView.TabIndex = 2;
-            this.PoleReg_PlotView.Text = "plotView1";
-            this.PoleReg_PlotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
-            this.PoleReg_PlotView.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
-            this.PoleReg_PlotView.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.zedGraphControl1.Location = new System.Drawing.Point(19, 49);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(1035, 548);
+            this.zedGraphControl1.TabIndex = 2;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
             // 
             // Plot_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 496);
-            this.Controls.Add(this.PoleReg_PlotView);
+            this.ClientSize = new System.Drawing.Size(1067, 610);
+            this.Controls.Add(this.zedGraphControl1);
             this.Controls.Add(this.PlotBasedOn_comboBox);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Plot_Form";
@@ -89,6 +96,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox PlotBasedOn_comboBox;
-        private OxyPlot.WindowsForms.PlotView PoleReg_PlotView;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
     }
 }
--- PlotForm.cs
+++ PlotForm.cs
@@ -1,8 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using OxyPlot;
-using OxyPlot.Series;
+using ZedGraph;
+using System.Drawing;
+//using OxyPlot;
+//using OxyPlot.Series;
 
 
 namespace ExtraTask
@@ -24,34 +26,77 @@ namespace ExtraTask
             MainFormObj = obj;
         }
 
+        
+        //public PlotModel model = new PlotModel { Title = "Polynomial Regression Plot" };
        
+
         private void PlotBasedOn_comboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            model.ResetAllAxes();
-            PlotModel model = new PlotModel { Title = "Plot" };
-            LineSeries dots = new LineSeries
-            {
-                LineStyle = LineStyle.None,
-                MarkerType = MarkerType.Circle,
-                MarkerSize = 1.5,
-                //MarkerStroke = OxyColors.Black
-            };
+            
+            //Func<double, double> func = x => coefForFunc[1] * x + coefForFunc[0];
 
-            PoleReg_PlotView.Model = model;
+            
 
-            double[] coefForFunc = Algorithm.PolynomialRegresion(PlotBasedOn_comboBox.SelectedItem.ToString(), InputDataList, out SortedList<double, double> variableForPlotsList);
 
-            Func<double, double> func = x => coefForFunc[1] * x + coefForFunc[0];
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.XAxis.Title.Text = "Time";
+            pane.YAxis.Title.Text = PlotBasedOn_comboBox.Text;
+            pane.Title.Text = "Polinomial regression";
 
-            model.Series.Add(new FunctionSeries(func, variableForPlotsList.Keys[0], variableForPlotsList.Keys[variableForPlotsList.Count - 1], 0.1));
+            pane.CurveList.Clear();
+            PointPairList dots = new PointPairList();
+            PointPairList func = new PointPairList(Algorithm.PolynomialRegresion(PlotBasedOn_comboBox.SelectedItem.ToString(), InputDataList, out SortedList<double, double> variableForPlotsList, 5));
 
+            LineItem myCurve = pane.AddCurve("Polynomial Regression", func, Color.Red, SymbolType.None );
+
+            //PointPairList dots = new PointPairList();
+            
             foreach (var pair in variableForPlotsList)
             {
-                dots.Points.Add(new DataPoint(pair.Key, pair.Value));
-                dots.Smooth = true;
+                dots.Add(pair.Key, pair.Value);
             }
-            model.Series.Add(dots);
-            PoleReg_PlotView.Model = model;
+
+            LineItem myDots = pane.AddCurve(null, dots, Color.Green, SymbolType.Circle);
+            myDots.Line.IsVisible = false;
+            myDots.Symbol.Fill.Color = Color.Green;
+            myDots.Symbol.Fill.Type = FillType.Solid;
+            myDots.Symbol.Size = 3;
+            //PlotModel model = new PlotModel { Title = "Polynomial Regression Plot"};
+
+
+            //LineSeries dots = new LineSeries
+            //{ 
+            //    //Color = OxyColors.Black,
+            //    LineStyle = LineStyle.None,              
+            //    MarkerType = MarkerType.Circle,
+            //    MarkerSize = 1.5,
+            //    //MarkerStroke = OxyColors.Black
+            //};
+            //var myController = new PlotController();
+            //PoleReg_PlotView.Controller = myController;
+
+            //PoleReg_PlotView.Model = model;
+
+            //double[] coefForFunc = Algorithm.PolynomialRegresion(PlotBasedOn_comboBox.SelectedItem.ToString(), InputDataList, out SortedList<double, double> variableForPlotsList);
+
+            //Func<double, double> func = x => coefForFunc[1] * x + coefForFunc[0];
+
+            //model.Series.Add(new FunctionSeries(func, variableForPlotsList.Keys[0], variableForPlotsList.Keys[variableForPlotsList.Count - 1], 0.1, "Polynomial Regression"));        
+
+            //foreach (var pair in variableForPlotsList)
+            //{
+            //    dots.Points.Add(new DataPoint(pair.Key, pair.Value));
+            //    dots.Smooth = true;
+
+            //}
+            //model.Series.Add(dots);
+            //PoleReg_PlotView.Model = model;
+            //pane.XAxis.Scale.Min = xmin;
+
+            //pane.YAxis.Scale.Min = ymin;
+
+            pane.AxisChange();
+            pane.AxisChange();
         }
 
         private void Plot_Form_Load(object sender, EventArgs e)
@@ -68,7 +113,7 @@ namespace ExtraTask
                 "Network received throughput [KB/s]",
                 "Network transmitted throughput [KB/s]"
             };
-            
+
             PlotBasedOn_comboBox.Items.AddRange(comboBoxComponent);
         }
 
